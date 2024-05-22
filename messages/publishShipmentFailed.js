76f9f77690f29de644a62b1c479f9f77a1313ecf
AugustHD <augu0577@stud.kea.dk>
2024-05-22 17:37:31 +0200

    Created PublisShipmentFailed.

---
+++ messages/publishShipmentFailed.js
@@ -0,0 +1,22 @@
+import { connectToRabbitMQ } from 'amqplib-retry-wrapper-dls';
+
+const channel = await connectToRabbitMQ(process.env.AMQP_HOST);
+
+async function publishShipmentFailed(message) {
+    const exchangeName = 'order_direct';
+    const exchangeType = 'fanout';
+    const routingKey = 'shipment_sent_failed'
+    
+    try {
+        await channel.assertExchange(exchangeName, exchangeType, {
+            durable: true
+        });
+
+        channel.publish(exchangeName, routingKey, Buffer.from(JSON.stringify(message)));
+        console.log('shipment_failed message published successfully');
+    } catch(error){
+        console.error('Error publishing shipment_failed message:', error);
+    }
+};
+
+export { publishShipmentFailed }
